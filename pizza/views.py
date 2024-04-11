8cfe94a8d5bcea7b2261361caa405ae2611b9d64
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-04-12 00:43:24 +0300

    update edit pizza order

--- pizza/views.py
+++ pizza/views.py
@@ -13,12 +13,13 @@ def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
-            filled_form.save()
+            created_pizza = filled_form.save()
+            created_pizza_pk = created_pizza.id
             note = 'Thanks for ordering! Your %s %s and %s pizza is on its way!' %(filled_form.cleaned_data['size'],
             filled_form.cleaned_data['topping1'],
             filled_form.cleaned_data['topping2'],)
             new_form = PizzaForm()
-            return render(request, "pizza/order.html", {'pizzaform':new_form, 'note':note, 'multiple_form': multiple_form})
+            return render(request, "pizza/order.html", {'created_pizza_pk': created_pizza_pk, 'pizzaform':new_form, 'note':note, 'multiple_form': multiple_form})
     else:
         form = PizzaForm()
         return render(request, "pizza/order.html", {'pizzaform':form, 'multiple_form': multiple_form})
@@ -42,3 +43,15 @@ def pizzas(request):
         return render(request, 'pizza/pizzas.html', {'note':note, 'formset':formset})
     else:
         return render(request, 'pizza/pizzas.html', {'formset':formset})
+
+def edit_order(request, pk):
+    pizza = Pizza.objects.get(pk=pk)
+    form = PizzaForm(instance=pizza)
+    if request.method =="POST":
+        filled_form = PizzaForm(request.POST, instance=pizza)
+        if filled_form.is_valid():
+            filled_form.save()
+            form = filled_form
+            note = "Your oder has been updated." #after edit the order
+            return render(request, 'pizza/edit_order.html', {'note':note,'pizzaform':form, 'pizza':pizza})
+    return render(request, 'pizza/edit_order.html', {'pizzaform':form, 'pizza':pizza})
