a2c76fe9bad7790e8113344b405024b23968525a
Sujan Adhikari <sujan.ad6473@gmail.com>
2024-04-11 16:52:33 +0300

    Project Pizza Hub

---
+++ pizza/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from .forms import PizzaForm
+
+def home(request):
+    return render(request, "pizza/home.html")
+
+def order(request):
+    form = PizzaForm()
+    return render(request, "pizza/order.html", {'pizzaform':form})
+    # return render(request, "pizza/order.html")
